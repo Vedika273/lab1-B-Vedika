d344d25066c0ac9f229eb9a3e8d70ba0e2feaffc
Vedika894 <vjain894@yahoo.com>
2025-08-31 14:19:45 -0400

    fixed the constructor in the FinalExam Class

--- src/lab1b/FinalExam.java
+++ src/lab1b/FinalExam.java
@@ -5,21 +5,27 @@
 package lab1b;
 
 /**
- *
+ * FinalExam is a graded activity that calculates score
+ * based on number of questions and how many were missed.
  * @author Vedika
  */
 public class FinalExam extends GradedActivity {
     private int numQuestions;
     private double pointsEach;
     private int numMissed;
-    private double numericScore;
     
     
     //constrcutor
     public FinalExam(int questions, int missed) {
+        
+        this.numQuestions = questions; 
+        this.numMissed = missed; 
+        
          pointsEach = 100.0 /questions; 
-         setScore(100.0 - (missed * pointsEach));
-         numericScore.set(100.0 -(missed * pointsEach));
+         double numericScore = 100.0 - (numMissed * pointsEach);
+         
+         //set the score as the new score calculated (numericScore
+         setScore(numericScore);
          
     }
     
