28c7ce2eec48a7093bd97676892ba40df136c2ce
Vedika894 <vjain894@yahoo.com>
2025-08-31 13:22:38 -0400

    add couseGrade methods names

---
+++ src/lab1b/CourseGrade.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab1b;
+
+/**
+ *
+ * @author Vedika
+ */
+public class CourseGrade {
+    GradedActivity grades[]; 
+    int Num_Grades = 4; 
+    
+    
+    public CourseGrade() {
+        
+    }
+    public void setLab() {
+        
+    }
+    
+    public void setPassFailExam() {
+        
+    }
+    
+    public void setEssay() {
+        
+    }
+    
+    public void setFinalExam() {
+        
+    }
+    
+    @Override
+    public String toString() {
+        
+    }
+}
--- src/lab1b/Essay.java
+++ src/lab1b/Essay.java
@@ -9,5 +9,46 @@ package lab1b;
  * @author Vedika
  */
 public class Essay extends GradedActivity{
+    private double grammar;
+    private double spelling; 
+    private double correctLength; 
+    private double content; 
     
-}
+    public void setScore(double gr, double sp, double len, double cnt) {
+        
+    }
+    
+    public void setGrammar(double g) {
+        this.grammar = g; 
+    }
+    
+    public void setSpelling (double s ) {
+        this.spelling = s; 
+    }
+    
+    public void setCorrectLength(double c ) {
+        this.correctLength = c; 
+    }
+    
+    public void setContent (double c ) {
+        this.content = c; 
+    }
+    
+    public double getGrammar() {
+        return grammar; 
+    }
+    
+    public double getSpelling() {
+        return spelling; 
+    }
+    
+    public double getCorrectLength() {
+        return correctLength;
+    }
+    
+    @Override
+    public double getScore() {
+       return getScore();
+    }
+ }
+
